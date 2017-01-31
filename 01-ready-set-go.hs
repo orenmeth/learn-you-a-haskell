ded8673adcdca57f4f0ef3c4bb90f59abe80c550
Oren Meth <orenmeth@gmail.com>
2017-01-31 23:04:49 +0200

    Feat: Getting started
    - add first steps

---
+++ 01-ready-set-go.hs
@@ -0,0 +1,38 @@
+{- Some simple arithmetic -}
+2 + 15
+
+49 * 100
+
+1892 - 1472
+
+5 / 2
+
+(50 * 100) - 4999
+
+50 * 100 - 4999
+
+50 * (100 - 4999)
+
+{- Surround negative numbers with parentheses -}
+5 * (-3)
+
+True && False
+
+True && True
+
+False || True
+
+not False
+
+not (True && True)
+
+{- Testing for equality -}
+5 == 5
+
+1 == 0
+
+5 /= 5
+
+5 /= 4
+
+"hello" == "hello"
